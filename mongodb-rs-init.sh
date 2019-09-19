199316afa715c599e6b0ca9efcca98b4503b01f7
Harry Tang <harry@powerkernel.com>
2019-09-19 09:19:59 +0300

    Update mongodb-rs-init.sh

--- mongodb-rs-init.sh
+++ mongodb-rs-init.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
 mkdir -p /data/mongodb/db
+mkdir -p /root/scripts
+curl -o /root/scripts/reload-mongodb.sh https://raw.githubusercontent.com/powerkernel/docker-mongodb-rs/master/reload-mongodb.sh
+chmod +x /root/scripts/reload-mongodb.sh
