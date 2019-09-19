d95cb2ef55a9015b105d9f398326f7138d9b660f
Harry Tang <harry@powerkernel.com>
2019-09-19 09:36:51 +0300

    Update mongodb-rs-init.sh

--- mongodb-rs-init.sh
+++ mongodb-rs-init.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 mkdir -p /data/mongodb/db
 mkdir -p /root/scripts
-mkdir -p /root/ssl/hostname
-mkdir -p /root/ssl/all
+mkdir -p /root/certs/hostname
+mkdir -p /root/certs/all
 curl -o /root/scripts/reload-mongodb.sh https://raw.githubusercontent.com/powerkernel/docker-mongodb-rs/master/reload-mongodb.sh
+curl -o /root/scripts/reload-hostname-cert.sh https://raw.githubusercontent.com/powerkernel/docker-mongodb-rs/master/reload-hostname-cert.sh
 chmod +x /root/scripts/reload-mongodb.sh
+chmod +x /root/scripts/reload-hostname-cert.sh
